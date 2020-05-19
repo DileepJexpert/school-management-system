774a5544e8e1517199f79ec8728559e4c98f79e9
DileepkMaurya <todileepmaurya@gmail.com>
2020-05-20 01:25:39 +0530

    ag

--- src/app/components/basic/typography/typography.component.ts
+++ src/app/components/basic/typography/typography.component.ts
@@ -15,8 +15,37 @@ export class TypographyComponent implements OnInit {
 
   students: Array<Student>;
   columnDefs=[
-    {headerName:'FirstName', field:'firstName', colId:'firstName', filter:'agTexxtFilter'}, 
-    {headerName:'LastName', field:'lastName', colId:'firstName', filter:'agTexxtFilter'} 
+    {headerName: 'adharNumber', field: 'adharNumber', sortable: true, filter: true, checkboxSelection: true, suppressSizeToFit: true,},
+    {headerName:'FirstName', field:'dateOfBirth', colId:'dateOfBirth', filter:'agTexxtFilter',suppressSizeToFit: true,}, 
+    {headerName:'LastName', field:'adharNumber', colId:'adharNumber', filter:'agTexxtFilter',suppressSizeToFit: true,} ,
+   
+
+            {headerName: 'Edit', field: 'studentName',  resizable: true ,width: 90,
+                cellRenderer : function(params){
+                    return '<div><button (click)="updateEmployee(student.id)" class="btn btn-info btn-sm" ><i class="fa fa-edit"></i>edit</button></div>'
+                }
+                
+            },
+
+            {headerName: 'Details', field: 'studentName',  resizable: true ,width: 90,
+                cellRenderer : function(params){
+                    return '<div><button (click)="employeeDetails(student._id)" class="btn btn-success btn-sm" ><i class="fa fa-info-circle"></i>info</button></div>'
+                }
+                
+            },
+
+            {headerName: 'TakeFee', field: 'studentName',  resizable: true ,width: 100,
+                cellRenderer : function(params){
+                    return '<div><button (click)="employeeDetails(student._id)" class="btn btn-secondary btn-sm" ><i class="fa fa-info-circle"></i>TakeFee</button></div>'
+                }
+                
+            },
+            {headerName: 'Delete', field: 'studentName',  resizable: true ,width: 90, suppressSizeToFit: true,
+            cellRenderer : function(params){
+                return '<button (click)="deleteEmployee(student.id)" class="btn btn-danger  btn-sm"><i class="fa fa-trash-o"></i></button></div>'
+            }
+            
+        }
   ];
 
   public rowData: Student[] = [];
@@ -30,6 +59,7 @@ export class TypographyComponent implements OnInit {
 
   reloadData() {
      this.studentService.getStudentList().subscribe((data) =>{
+       console.log("---------"+data);
       this.rowData = data;
       this.students = data;
     });
